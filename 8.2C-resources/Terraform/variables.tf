276dfb2acd1c3d3dd0610c6061ccdcc168d46296
Akhila Joseph <43929349+AkhilaJoseph@users.noreply.github.com>
2024-09-19 18:17:59 +1000

    Add files via upload

---
+++ 8.2C-resources/Terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "my-resource-group"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "acr_name_AJ" {
+  default = "myacraj123" 
+}
+
+variable "kubernetes_cluster_name" {
+  default = "myAKSCluster"
+}
+
+variable "dns_prefix" {
+  default = "myakscluster"
+}
